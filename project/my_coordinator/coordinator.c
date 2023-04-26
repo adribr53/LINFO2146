aab1da731f6d50368e9dedeb370294f2451cde75
Felix <felix.gaudin@student.uclouvain.be>
2023-04-26 16:05:29 +0200

    coordinator + sensor (not working)

---
+++ project/my_coordinator/coordinator.c
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2006, Swedish Institute of Computer Science.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ */
+
+/**
+ * \file
+ *         A very simple Contiki application showing how Contiki programs look
+ * \author
+ *         Adam Dunkels <adam@sics.se>
+ */
+
+#include "contiki.h"
+#include "dev/serial-line.h"
+#include "cpu/msp430/dev/uart0.h"
+
+#include "net/netstack.h"
+#include "net/nullnet/nullnet.h"
+
+#include <stdio.h> /* For printf() */
+
+/*---------------------------------------------------------------------------*/
+PROCESS(test_serial, "Coordinator node");
+AUTOSTART_PROCESSES(&test_serial);
+/*---------------------------------------------------------------------------*/
+
+#define MAX_PAYLOAD_LENGTH 42
+
+typedef enum {
+  SENSOR_NODE = 0,
+  COORDINATOR_NODE = 1,
+  BORDER_NODE = 2,
+  UNDEFINED_NODE = 3
+} node_type;
+
+typedef enum {
+  DISCOVERY_TYPE = 0,
+  MESSAGE_TYPE = 1,
+} packet_type;
+
+typedef struct packet {
+  node_type type;
+  node_type snd;
+  uint8_t payload[MAX_PAYLOAD_LENGTH];
+} packet_t;
+
+
+#define OWN_TYPE COORDINATOR_NODE
+
+static linkaddr_t parent;
+struct etimer parent_last_update; // TODO: maybe another type
+
+void input_callback(const void *data, uint16_t len, const linkaddr_t *src, const linkaddr_t *dest) {
+  if (!linkaddr_cmp(src, &linkaddr_node_addr) && len == sizeof(packet_t)) {
+    packet_t pkt;
+    memcpy(&pkt, data, sizeof(packet_t));
+    switch (pkt.type)
+    {
+    case DISCOVERY_TYPE:
+      if (pkt.snd == BORDER_NODE) {
+        // TODO: parent = src
+      } // else discard
+      break;
+    case MESSAGE_TYPE:
+      // If have parent => fw
+      break;
+    default:
+      // Discard
+      break;
+    }
+  }
+}
+
+
+
+PROCESS_THREAD(test_serial, ev, data) {
+
+  //static struct etimer timer;
+
+  PROCESS_BEGIN();
+
+  /* Initialize NullNet */
+  nullnet_buf = NULL;
+  nullnet_len = 0;
+  nullnet_set_input_callback(input_callback);
+
+  //etimer_set(&timer, CLOCK_SECOND * 1000);
+  printf("Starting process\n");
+  while(1) {
+    // TODO : send discovery when actived
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
---
+++ project/my_sensor/sensor.c
@@ -0,0 +1,158 @@
+/*
+ * Copyright (c) 2006, Swedish Institute of Computer Science.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ */
+
+/**
+ * \file
+ *         A very simple Contiki application showing how Contiki programs look
+ * \author
+ *         Adam Dunkels <adam@sics.se>
+ */
+
+#include "contiki.h"
+#include "dev/serial-line.h"
+#include "cpu/msp430/dev/uart0.h"
+#include "net/netstack.h"
+#include "net/nullnet/nullnet.h"
+#include "coucou.h"
+
+#include "dev/button-sensor.h"
+
+#include <stdio.h> /* For printf() */
+
+#include <stdlib.h>
+#include <string.h>
+#include <radio.h>
+/*---------------------------------------------------------------------------*/
+PROCESS(test_serial, "Sensor node");
+AUTOSTART_PROCESSES(&test_serial);
+/*---------------------------------------------------------------------------*/
+
+#define MAX_PAYLOAD_LENGTH 42
+
+typedef enum {
+  SENSOR_NODE = 0,
+  COORDINATOR_NODE = 1,
+  BORDER_NODE = 2,
+  UNDEFINED_NODE = 3
+} node_type;
+
+typedef enum {
+  DISCOVERY_TYPE = 0,
+  MESSAGE_TYPE = 1,
+} packet_type;
+
+typedef struct packet {
+  node_type type;
+  node_type snd;
+  uint8_t payload[MAX_PAYLOAD_LENGTH];
+} packet_t;
+
+
+#define OWN_TYPE SENSOR_NODE;
+static linkaddr_t parent;
+static node_type parent_type;
+static linkaddr_t* child_nodes;
+radio_value_t  parent_strength;
+struct etimer parent_last_update;
+
+void input_callback(const void *data, uint16_t len, const linkaddr_t *src, const linkaddr_t *dest) {
+  if (!linkaddr_cmp(src, &linkaddr_node_addr) && len == sizeof(packet_t)) {
+    packet_t pkt;
+    memcpy(&pkt, data, sizeof(packet_t));
+    if (pkt.type == DISCOVERY_TYPE) {
+      if (pkt.snd == COORDINATOR_NODE) {
+          if (parent_type != COORDINATOR_NODE) {
+            // old parent : unkown or sensor
+            // TODO:parent = src
+            // maybe : memcpy(parent, src, sizeof(linkaddr_t));
+            parent_type = COORDINATOR_NODE;
+            parent_strength = get_strength();
+            // TODO: parent_last_update = now()
+          } else {
+              // compare strengt
+              radio_value_t new_strength = get_strength();
+              if (new_strength > parent_strength) {
+                // TODO: parent = src
+                parent_strength = new_strength
+                // TODO: parent_last_update = now()
+              }
+          }
+      } else if (pkt.snd == SENSOR_NODE) {
+          if (parent_type != COORDINATOR_NODE) {
+              if (parent_type != UNDEFINED_NODE) {
+                  // already have a parent
+                  radio_value_t new_strength = get_strength();
+                  if (new_strength > parent_strength) {
+                      // TODO: parent = src
+                      parent_strength = new_strength;
+                      // TODO: parent_last_update = now()
+                  }
+              } else {
+                  // TODO: parent = src
+                  parent_type = SENSOR_NODE;
+                  parent_strength = get_strength();
+                  // TODO: parent_last_update = now()
+              }
+          } // else discard
+      } // else border or bug
+  }
+  }
+}
+
+PROCESS_THREAD(test_serial, ev, data)
+{
+  static unsigned count = 0;
+  static char* msg = "HELLO";
+  //static struct etimer timer;
+
+  PROCESS_BEGIN();
+
+  /* Initialize NullNet */
+  nullnet_buf = &msg;
+  nullnet_len = 6;
+  nullnet_set_input_callback(input_callback);
+
+  //etimer_set(&timer, CLOCK_SECOND * 1000);
+  printf("Starting process\n");
+  while(1) {
+    PROCESS_WAIT_EVENT_UNTIL((ev == sensors_event) && (data == &button_sensor));
+    // TODO: Wait timer 
+    // To send discovery & send data to parent
+    // memcpy(nullnet_buf, &msg, sizeof(count));
+    // nullnet_len = 6;
+
+    // NETSTACK_NETWORK.output(NULL);
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
