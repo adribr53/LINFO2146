3e765e8e2ffcd3498027f2774d605ed6782417f4
adribr53 <adrien.giot@student.uclouvain.be>
2023-04-29 18:48:26 +0200

    border and coordinator became friends on linkedin

--- project/my_coordinator/coordinator.c
+++ project/my_coordinator/coordinator.c
@@ -1,63 +1,25 @@
-/*
- * Copyright (c) 2006, Swedish Institute of Computer Science.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * This file is part of the Contiki operating system.
- *
- */
-
-/**
- * \file
- *         A very simple Contiki application showing how Contiki programs look
- * \author
- *         Adam Dunkels <adam@sics.se>
- */
-
 #include "contiki.h"
-#include "dev/serial-line.h"
-#include "cpu/msp430/dev/uart0.h"
-
 #include "net/netstack.h"
 #include "net/nullnet/nullnet.h"
-#include "dev/button-sensor.h"
-
-#include <stdlib.h>
+#include "dev/serial-line.h"
+#include "cpu/msp430/dev/uart0.h"
 #include <string.h>
-#include <radio.h>
+#include <stdio.h> /* For printf() */
 
+/* Log configuration */
 #include "sys/log.h"
 #define LOG_MODULE "App"
 #define LOG_LEVEL LOG_LEVEL_INFO
-/*---------------------------------------------------------------------------*/
-PROCESS(nullnet_example_process, "Coordinator node");
-AUTOSTART_PROCESSES(&nullnet_example_process);
-/*---------------------------------------------------------------------------*/
 
-#define MAX_PAYLOAD_LENGTH 42
+/* Configuration */
+#define SEND_INTERVAL (8 * CLOCK_SECOND)
+
+#if MAC_CONF_WITH_TSCH
+#include "net/mac/tsch/tsch.h"
+static linkaddr_t coordinator_addr =  {{ 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }};
+#endif /* MAC_CONF_WITH_TSCH */
+
+//static linkaddr_t dest_addr =  {{ 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }};
 
 typedef enum {
   SENSOR_NODE = 0,
@@ -72,35 +34,40 @@ typedef enum {
 } packet_type;
 
 typedef struct packet {
-  node_type type;
-  node_type snd;
-  uint8_t payload[MAX_PAYLOAD_LENGTH];
+  node_type node : 2;
+  packet_type msg : 1;
+  uint8_t payload : 5;
 } packet_t;
 
-
-#define OWN_TYPE COORDINATOR_NODE
-
 static linkaddr_t parent;
-// struct etimer parent_last_update; // TODO: maybe another type
-packet_t to_send;
+static unsigned has_parent = 0;
+/*---------------------------------------------------------------------------*/
+PROCESS(nullnet_example_process, "NullNet broadcast example");
+AUTOSTART_PROCESSES(&nullnet_example_process);
 
-void input_callback(const void *data, uint16_t len, const linkaddr_t *src, const linkaddr_t *dest) {
-  if (!linkaddr_cmp(src, &linkaddr_node_addr) && len == sizeof(packet_t)) {
-    packet_t pkt;
+/*---------------------------------------------------------------------------*/
+void input_callback(const void *data, uint16_t len,
+  const linkaddr_t *src, const linkaddr_t *dest)
+{
+  if(len == sizeof(packet_t)) {    
+    static packet_t pkt;
     memcpy(&pkt, data, sizeof(packet_t));
     LOG_INFO("Received from ");
     LOG_INFO_LLADDR(src);
     LOG_INFO_("\n");
-    switch (pkt.type)
+    switch (pkt.msg)
     {
     case DISCOVERY_TYPE:
       LOG_INFO("Discovery\n");
-      if (pkt.snd == BORDER_NODE) {
+      if (pkt.node == BORDER_NODE) {
+        LOG_INFO("From border");
         memcpy(&parent, src, sizeof(linkaddr_t));
+        has_parent = 1;
         // TODO: parent_last_update = now()
       } else {
-        if (pkt.snd == SENSOR_NODE) LOG_INFO("From sensor\n");
-        if (pkt.snd == COORDINATOR_NODE) LOG_INFO("From coordinator\n");
+        if (pkt.node == SENSOR_NODE) LOG_INFO("From sensor\n");
+        if (pkt.node == COORDINATOR_NODE) LOG_INFO("From coordinator\n");
+        if (pkt.node == UNDEFINED_NODE) LOG_INFO("From undefined\n");
       }
       break;
     case MESSAGE_TYPE:
@@ -111,35 +78,58 @@ void input_callback(const void *data, uint16_t len, const linkaddr_t *src, const
       // Discard
       break;
     }
+
   }
+  LOG_INFO_("\n");
+
 }
-
-PROCESS_THREAD(nullnet_example_process, ev, data) {
-
-  //static struct etimer timer;
-
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(nullnet_example_process, ev, data)
+{
+  static struct etimer periodic_timer;  
+  static packet_t discovery_pkt;
+  static unsigned count = 0;  
+    
+  
   PROCESS_BEGIN();
-  SENSORS_ACTIVATE(button_sensor);
-  static packet_t discorvery_pkt;
-  discorvery_pkt.snd = OWN_TYPE;
-  discorvery_pkt.type = DISCOVERY_TYPE;
+
+#if MAC_CONF_WITH_TSCH
+  tsch_set_coordinator(linkaddr_cmp(&coordinator_addr, &linkaddr_node_addr));
+#endif /* MAC_CONF_WITH_TSCH */
+
   /* Initialize NullNet */
-  nullnet_buf = (uint8_t *)&discorvery_pkt;
-  nullnet_len = sizeof(packet_t);
+  nullnet_buf = (uint8_t *)&discovery_pkt;
+  nullnet_len = sizeof(discovery_pkt);
   nullnet_set_input_callback(input_callback);
 
-  //etimer_set(&timer, CLOCK_SECOND * 1000);
-  printf("Starting process\n");
-  LOG_INFO("Type : Coordinator\n");
+  etimer_set(&periodic_timer, SEND_INTERVAL);
+  
+  // sends a dummy pkt to prevent bug
+  discovery_pkt.node = UNDEFINED_NODE;
+  discovery_pkt.msg = DISCOVERY_TYPE;
+  NETSTACK_NETWORK.output(&linkaddr_node_addr);
+  
+  discovery_pkt.node = COORDINATOR_NODE;
+  discovery_pkt.msg = DISCOVERY_TYPE;
+  discovery_pkt.payload = count;
   while(1) {
-    PROCESS_WAIT_EVENT_UNTIL((ev == sensors_event) && (data == &button_sensor));
-    LOG_INFO("Sending discovery broadcast\n");
-    nullnet_buf = (uint8_t *)&discorvery_pkt;
-    nullnet_len = sizeof(packet_t);
-    NETSTACK_NETWORK.output(NULL);
-    // TODO : send discovery when actived
+    /* SEND SIGNALING MSG "I AM THE BORDER" */
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
+    if (has_parent) {
+      LOG_INFO("Sending pkt of type %u with payload %u to ", discovery_pkt.node, discovery_pkt.payload);
+      LOG_INFO_LLADDR(NULL);
+      LOG_INFO_("\n");      
+      memcpy(nullnet_buf, &discovery_pkt, sizeof(discovery_pkt));
+      nullnet_len = sizeof(discovery_pkt);      
+      NETSTACK_NETWORK.output(&parent);
+      discovery_pkt.payload++;
+    }
+    /* RESET TIMER */
+    etimer_reset(&periodic_timer);
   }
 
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
+
+
